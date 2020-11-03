fe0aed4a0e4176841f57302626a08c1ee3a2c32b
AYA-kurokiri09 <ice6e63@gmail.com>
2020-11-03 23:15:40 +0900

    main.js追加

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+	setTimeout(function(){
+		$('.start-inner').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},1500); //1.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
